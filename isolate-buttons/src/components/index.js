9d46b587bdc29cc8bb76212e87ed8b4ef83a31e1
Vincent Orr <Vincent@cmdv.org>
2016-03-22 22:22:13 +0000

    changing structure

--- isolate-buttons/src/components/index.js
+++ isolate-buttons/src/components/index.js
@@ -3,43 +3,31 @@ import {h, div, h1, h2, button, p} from '@cycle/dom'
 import isolate from '@cycle/isolate'
 import isolateButton from './isolate-button'
 
-const view = (state$) =>
-  state$.map(state =>
-    div('.homepage', [
+
+function view (state$) {
+  return state$.map(buttonView);
+}
+
+function buttonView ({buttons, colour}) {
+  return (
+    div('.buttons', [
       h1([`Isolate Buttons`]),
-      div([
-        div([state.map((b) => {
-          return div({style: {'background': 'lightgrey', 'margin': '15px', 'padding': '10px', 'width': '25%', 'float': 'left'}},[
-            button('.button', {style: {'padding': '5px', 'margin': '5px'}}, 'Add New | ' + b),
-            button('.delete', {style: {'padding': '5px', 'margin': '5px', 'float': 'right'}}, 'delete')
-          ])
-          // return div([isolate(isolateButton)(b)])
-        })
-        ])
-      ])
+      buttons.map(buttons => buttons.DOM)
     ])
   );
+}
 
-const intent = s => ({
-  inc$: s.DOM.select('.button').events('click').map(ev => 1),
-});
-
-const model = ({inc$, props$}) => {
-  return Rx.Observable.merge(
-    props$.take(1).map((count) => count),
-    inc$,
-    )
-    .scan((x, y) => x.concat([y]))
-    .shareReplay(1)
+const model = () => {
+  // ???
 }
 
 const main = (sources) => {
-  const props$ = sources.Props;
-  const actions = intent(sources);
-  const state$ = model({...actions,props$});
+  const props$ = sources.Props 
+  const isolateButton$ = isolate(isolateButton)(/*???*/)
+  const state$ = model({...actions, props$});
 
   return {
-    DOM: view(state$),
+    DOM: view(state$, isolateButton$),
     Props: state$,
   }
 };
--- isolate-buttons/src/components/isolate-button.js
+++ isolate-buttons/src/components/isolate-button.js
@@ -1,16 +1,48 @@
-import {button} from '@cycle/dom'
+import {h, div, h1, h2, button, p} from '@cycle/dom'
 
 const view = (state$) =>
   state$.map(state =>
-    button('.button', {style: {'padding': '10px'}}, 'Click me to duplicate'),
+    div(
+      {style:
+        { 'background': state.colour,
+          'margin': '15px',
+          'padding': '10px',
+          'width': '25%',
+          'float': 'left'
+        }
+      },
+      [button('.add',
+        {style: {'padding': '5px', 'margin': '5px'}},
+        ['Add New | ' + b]),
+      button('.delete',
+        {style: {'padding': '5px', 'margin': '5px', 'float': 'right'}},
+        ['delete'])
+      ])
   );
 
 
-const isolateButton = (state$) => {
-  return {
-    DOM: view(state$),
-    Props: state$,
-  }
-};
+const intent = ({DOM}) => ({
+  addButton$: DOM.select('.add').events('click')
+    .map(() => ({ colour : '#'+ Math.floor(Math.random()*16777215).toString(16)}) ),
+});
 
-export default isolateButton
+const model = (props, actions$) => {
+  return props.merge(actions$)
+    .startWith({})
+    // ????
+    .scan((state, change) => Object.assign({}, state, change))
+    .shareReplay(1);
+}
+
+function Buttons ({DOM, props}) {
+  const actions$ = intent(DOM)
+  const state$ = model(props, actions$)
+  const vtree$ = state$.map(view)
+
+  return {
+    DOM: vtree$,
+    Pops: state$
+  };
+}
+
+export default Buttons
