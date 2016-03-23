de387ba8c7f222d044778ceba7416373cf1d31b5
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-03-23 04:15:39 -0400

    refactor(): general polishing

--- isolated-folder/src/Folder.js
+++ isolated-folder/src/Folder.js
@@ -6,9 +6,10 @@ import combineLatestObj from 'rx-combine-latest-obj'
 const ADD = id => ({id, type: 'add'})
 const REMOVE = id => ({id, type: 'remove'})
 
-function intent({DOM, action$}, id) {
+function intent({DOM}, action$, id) {
   const add$ = DOM.select('.add').events('click').map(() => ADD(id))
   const remove$ = DOM.select('.remove').events('click').map(() => REMOVE(id))
+    .share()
   const removeFolder$ = action$.filter(action => action.type === 'remove')
   return {add$, remove$, removeFolder$}
 }
@@ -34,40 +35,45 @@ function findFolder(children, _id) {
 }
 
 function model(actions, sources, parentId) {
-  const newFolderMod$ = actions.add$.map(() => children => {
-    const lastId = children.length > 0 ?
-      parseInt(children[children.length - 1].id) : 0
-    const Folder = isolate(createFolder({id: `${parentId}${lastId + 1}`}))
-    const proxyAction$ = new Subject()
-    const folder = Folder({action$: proxyAction$, ...sources})
-    const {action$, remove$} = folder
-    action$.takeUntil(remove$).subscribe(proxyAction$.asObserver())
-    return children.concat(folder)
-  })
+  const newFolderMod$ = actions.add$
+    .map(() =>
+      function(children) {
+        const lastId = children.length > 0 ?
+          parseInt(children[children.length - 1].id) : 0
+        const Folder = createFolderComponent({id: `${parentId}${lastId + 1}`})
+        const folder = isolate(Folder)(sources)
+        return children.concat(folder)
+      }
+    )
 
-  const removeFolderMod$ = actions.removeFolder$.map(({id}) => children => {
-    const folderIndex = findFolder(children, id)
-    if (folderIndex !== null) {
-      children.splice(folderIndex, 1)
-    }
-    return children
-  })
+  const removeFolderMod$ = actions.removeFolder$
+    .map(({id}) =>
+      function(children)  {
+        const folderIndex = findFolder(children, id)
+        if (folderIndex !== null) {
+          children.splice(folderIndex, 1)
+        }
+        return children
+      }
+    )
 
   const children$ = newFolderMod$
     .merge(removeFolderMod$)
     .startWith([])
     .scan((children, modFn) => modFn(children))
 
-  return combineLatestObj({children$}).shareReplay(1)
+  return children$.shareReplay(1)
 }
 
-const style = (color) => ({
-  backgroundColor: color,
-  padding: '2em',
-  width: 'auto',
-  border: '2px solid black',
-  transition: 'all 0.6s ease-in-out'
-})
+function style(color) {
+  return {
+    backgroundColor: color,
+    padding: '2em',
+    width: 'auto',
+    border: '2px solid black',
+    transition: 'all 0.6s ease-in-out',
+  }
+}
 
 function makeRandomColor() {
   let hexColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -78,36 +84,39 @@ function makeRandomColor() {
   return hexColor;
 }
 
-const view = (removable, color) => ({children}) => {
-  return div({style: style(color)}, [
-    button('.add', ['Add Folder']),
-    removable && button('.remove', ['Remove me']),
-    children && div({}, children.map(({id, DOM}) =>
-      div({key: id}, [DOM])
-    ))
-  ])
+function makeView(removable, color) {
+  return function view(children) {
+    return div({style: style(color)}, [
+      button('.add', ['Add Folder']),
+      removable && button('.remove', ['Remove me']),
+      children && div({}, children.map(({id, DOM}) =>
+        div({key: id}, [DOM])
+      ))
+    ])
+  }
 }
 
-function createFolder({id, removable = true}) {
+function createFolderComponent({id, removable = true}) {
   return function Folder(sources) {
-    const actions = intent(sources, id)
+    const proxyAction$ = new Subject()
+    const actions = intent(sources, proxyAction$, id)
     const state$ = model(actions, sources, id)
     const color = makeRandomColor()
-    const view$ = state$.map(view(removable, color))
+    const view$ = state$.map(makeView(removable, color))
 
     const action$ = state$
-      .flatMapLatest(
-        ({children}) => Observable.merge(children.map(c => c.action$))
-      )
+      .flatMapLatest(children => Observable.merge(children.map(c => c.action$)))
       .merge(actions.remove$)
+      .share()
+
+    action$.takeUntil(actions.remove$).subscribe(proxyAction$.asObserver())
 
     return {
       DOM: view$,
-      remove$: actions.remove$,
       action$,
       id,
     }
   }
 }
 
-export {createFolder}
+export {createFolderComponent}
--- isolated-folder/src/app.js
+++ isolated-folder/src/app.js
@@ -2,15 +2,11 @@ import {Subject} from 'rx'
 import {run} from '@cycle/core'
 import {makeDOMDriver} from 'cycle-snabbdom'
 
-import {createFolder} from './Folder'
+import {createFolderComponent} from './Folder'
 
 function main(sources) {
-  const Folder = createFolder({id: '0', removable: false})
-  const proxyAction$ = new Subject()
-  const folder = Folder({action$: proxyAction$, ...sources})
-  const {action$, remove$} = folder
-  action$.takeUntil(remove$).subscribe(proxyAction$.asObserver())
-  return folder
+  const Folder = createFolderComponent({id: '0', removable: false})
+  return Folder(sources)
 }
 
 run(main, {DOM: makeDOMDriver('#app')})
