60bf9eb23fc297b6d9327b93cf1d33ecf996408e
Nick Johnstone <ncwjohnstone@gmail.com>
2016-06-19 23:19:58 +0900

    Convert nested-folders example to use @cycle/collection

--- nested-folders/src/Folder.js
+++ nested-folders/src/Folder.js
@@ -1,41 +1,13 @@
 import xs from 'xstream'
-import isolate from '@cycle/isolate'
 import {div, button} from '@cycle/dom'
+import Collection from '@cycle/collection'
 
-function intent(DOMSource, childAction$, selfId) {
-  return xs.merge(
-    DOMSource.select('.add').events('click')
-      .mapTo({selfId, type: 'addChild'}),
+function intent(DOMSource) {
+  return {
+    addChild$: DOMSource.select('.add').events('click').mapTo(null),
 
-    DOMSource.select('.remove').events('click')
-      .mapTo({selfId, type: 'removeSelf'}),
-
-    childAction$
-      .filter(action => action.type === 'removeSelf')
-      .map(action => ({...action, type: 'removeChild'}))
-  )
-}
-
-function model(action$, createIsolatedFolder) {
-  const addFolderUpdate$ = action$
-    .filter(({type}) => type === 'addChild')
-    .map(action => function addFolderUpdate(childrenMap) {
-      const childId = String(Math.random()).replace('0.', '')
-      const folder = createIsolatedFolder(childId)
-      return childrenMap.set(childId, folder)
-    })
-
-  const removeFolderUpdate$ = action$
-    .filter(({type}) => type === 'removeChild')
-    .map(action => function removeFolderUpdate(childrenMap) {
-      childrenMap.delete(action.selfId)
-      return childrenMap
-    })
-
-  const children$ = xs.merge(addFolderUpdate$, removeFolderUpdate$)
-    .fold((children, update) => update(children), new Map())
-
-  return children$
+    remove$: DOMSource.select('.remove').events('click')
+  }
 }
 
 function style(backgroundColor) {
@@ -52,9 +24,7 @@ function makeView(removable, color) {
     return div({style: style(color)}, [
       button('.add', ['Add Folder']),
       removable && button('.remove', ['Remove me']),
-      children && div({}, Array.from(children.values()).map(child =>
-        div({key: child.id}, [child.DOM])
-      ))
+      div(children)
     ])
   }
 }
@@ -67,37 +37,28 @@ function makeRandomColor() {
   return '#' + hexColor
 }
 
-function createFolderComponent({id, removable = true}) {
+function createFolderComponent({removable = true}) {
   function Folder(sources) {
-    function createFolder(childId) {
-      return createFolderComponent(({id: childId}))(sources)
-    }
+    const actions = intent(sources.DOM)
 
-    const proxyChildAction$ = xs.create();
-    const action$ = intent(sources.DOM, proxyChildAction$, id)
-    const children$ = model(action$, createFolder)
+    const children$ = Collection(
+      createFolderComponent({}),
+      sources,
+      actions.addChild$
+    )
+
     const color = makeRandomColor()
-    const vdom$ = children$.map(makeView(removable, color))
-    const removeSelf$ = action$.filter(({type}) => type === 'removeSelf')
 
-    const childAction$ = children$
-      .map(children => {
-        const childActionStreams = Array.from(children.values())
-          .map(c => c.action$)
-        return xs.merge(...childActionStreams)
-      })
-      .flatten()
-
-    proxyChildAction$.imitate(childAction$.endWhen(removeSelf$.take(1)))
+    const vdom$ = Collection.pluck(children$, 'DOM')
+      .map(makeView(removable, color))
 
     return {
       DOM: vdom$,
-      action$,
-      id,
+      remove$: actions.remove$
     }
   }
 
-  return isolate(Folder)
+  return Folder
 }
 
 export {createFolderComponent}
